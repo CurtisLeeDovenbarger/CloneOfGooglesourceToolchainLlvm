CurtisLeeDovenbarger/CloneOfGooglesourceToolchainLlvm

commit 4331826dc942c593d093c9cf46e122e78f26ec15

Workaround for metadata corrupted issue.
When building ndk tests issue52819-STLPORT_FORCE_REBUILD and test-googletest-full,
An assertion happend in llvm_shutdown:

  While deleting: metadata %
  An asserting value handle still pointed to this value!

Use the patch below to workaround.
https://codereview.chromium.org/10442019/

Change-Id: Ie3cd78e11f3a91fab5bfda2d3f830a94051f4e73

include/llvm/CodeGen/LexicalScopes.h
@@ -162,6 +162,8 @@ public:
 #ifndef NDEBUG
     IndentLevel = 0;
 #endif
+    Desc.make_weak();
+    InlinedAtLocation.make_weak();
     if (Parent)
       Parent->addChild(this);
   }

include/llvm/Support/ValueHandle.h
@@ -104,6 +104,8 @@ protected:
   void setValPtrInt(unsigned K) { VP.setInt(K); }
   unsigned getValPtrInt() const { return VP.getInt(); }
 
+  void setKind(HandleBaseKind K) { PrevPair.setInt(K); }
+
   static bool isValid(Value *V) {
     return V &&
            V != DenseMapInfo<Value *>::getEmptyKey() &&
@@ -221,6 +223,10 @@ public:
 
   operator ValueTy*() const {
     return getValPtr();
+  }
+
+  void make_weak() {
+    setKind(Weak);
   }
 
   ValueTy *operator=(ValueTy *RHS) {

commit 531aca0d308f4f9391e67b5876105d8009bc6060

Fix compile error when assertion turned off.
This bug is introduced by 4331826dc9.
The error only occurs when we configure --enable-assertions=no.

Change-Id: Ia9d5ad3637ece3535e7478838aa72e8b8e4632a8
Signed-off-by: WenHan Gu <Wenhan.gu@mediatek.com>

include/llvm/CodeGen/LexicalScopes.h
@@ -161,9 +161,9 @@ public:
       LastInsn(0), FirstInsn(0), DFSIn(0), DFSOut(0) {
 #ifndef NDEBUG
     IndentLevel = 0;
-#endif
     Desc.make_weak();
     InlinedAtLocation.make_weak();
+#endif
     if (Parent)
       Parent->addChild(this);
   }

include/llvm/Support/ValueHandle.h
@@ -225,9 +225,11 @@ public:
     return getValPtr();
   }
 
+#ifndef NDEBUG
   void make_weak() {
     setKind(Weak);
   }
+#endif
 
   ValueTy *operator=(ValueTy *RHS) {
     setValPtr(RHS);