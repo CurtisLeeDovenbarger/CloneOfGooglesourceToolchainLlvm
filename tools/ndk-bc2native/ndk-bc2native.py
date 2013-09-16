2c62eedf895a789a5addef860aabe4a08a4b5127
Andrew Hsieh <andrewhsieh@google.com>
2013-09-16 18:14:20 +0800

    fix float-abi
    "-float-abi soft" for armeabi and armeabi-v7a, unspecified for the rest
    
    Change-Id: Ia57e43a4f893fb9c5794ed590cf74ebdbba0590b

--- tools/ndk-bc2native/ndk-bc2native.py
+++ tools/ndk-bc2native/ndk-bc2native.py
@@ -342,10 +342,11 @@ def do_as(asm, relocatable):
     global ABI
 
     args = [AS_CMD]
-    args += ['-mfloat-abi=soft']
     if ABI == 'armeabi':
+        args += ['-mfloat-abi=soft']
         args += ['-march=armv5te']
     elif ABI == 'armeabi-v7a':
+        args += ['-mfloat-abi=soft']
         args += ['-march=armv7-a']
         args += ['-mfpu=vfpv3-d16']
 
@@ -386,7 +387,8 @@ def do_llc(bitcode, output):
     args += ['-code-model=small']
     args += ['-use-init-array']
     args += ['-mc-relax-all']
-    args += ['-float-abi=soft']
+    if ABI == 'armeabi' or ABI == 'armeabi-v7a':
+        args += ['-float-abi=soft']
     args += get_llc_flags_for_abi(ABI)
     args += ['-O2']
     args += [bitcode]
